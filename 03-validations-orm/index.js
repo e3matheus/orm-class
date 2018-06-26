7317a17bc3387ef0e82ad9dc342bd6e49c29bae4
Elías Matheus <e3matheus@gmail.com>
2018-06-26 10:02:44 -0500

    Fix indentation

--- 03-validations-orm/index.js
+++ 03-validations-orm/index.js
@@ -12,8 +12,6 @@ const sequelize = new Sequelize('database', 'username', 'password', {
     acquire: 30000,
     idle: 10000
   },
-
-	// logging: false,
 });
 
 // Validations
@@ -26,17 +24,17 @@ const Movie = sequelize.define('movie', {
 Movie.sync().then(() => {
   let noName = Movie.build({
     name: "",
-		description: 'The Avengers and their allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe.',
+    description: 'The Avengers and their allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe.',
     active: true
-	});
+  });
 
   noName.save().then( (movie) => console.log(movie) ).catch(error => console.log(error.errors[0].messages));
 
   let badNameMovie = Movie2.build({
     name: "Night of the Day of the Dawn of the Son of the Bride of the Return of the Revenge of the Terror of the Attack of the Evil, Mutant, Hellbound, Flesh-Eating Subhumanoid Zombified Living Dead, Part 3",
-		description: 'Worst movie ever',
+    description: 'Worst movie ever',
     active: true
-	});
+  });
 
   noName.save().then( (movie) => console.log(movie) ).catch(error => console.log(error.errors[0].message));
 });
